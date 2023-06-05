37678497fa176c7729aca3dba3377299b0ce7e1a
Chase Helgenlechner <chase@helgenlechner.com>
2023-06-05 11:48:11 +0200

    Created three projects:  - WPF Application  - C# Class Library  - NUnit Test Library Both the WPF App and the NUnit Library reference the C# Class Library.

---
+++ UTILITIES/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace UTILITIES
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ UTILITIES_TESTS/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UTILITIES_TESTS
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
