57259497be9940a4b252fc71e5819ecb9b0ee9f1
Chase Helgenlechner <chase@helgenlechner.com>
2023-06-07 14:49:18 +0200

    Database context has been established. Database currently only contains a results table. The goal for now is to read a results file into the database via a WPF application.

---
+++ Database/LV_Categorisation Database - Setup.sql
@@ -0,0 +1,34 @@
+USE MASTER
+DROP DATABASE LV_CATEGORISATION
+
+CREATE DATABASE LV_CATEGORISATION
+GO
+
+USE LV_CATEGORISATION
+
+----DROP TABLE CATEGORIES
+--CREATE TABLE CATEGORIES (
+--	id int PRIMARY KEY IDENTITY(1,1),
+--	Name varchar(50) NOT NULL
+--	)
+
+--SELECT * FROM CATEGORIES
+
+
+--DROP TABLE RESULTS
+CREATE TABLE RESULTS (
+	id int PRIMARY KEY IDENTITY(1,1),
+	Positionsnummer varchar(20),
+	Kurztext varchar(1000),
+	Menge decimal,
+	Einheiten varchar(20),
+	Langtext varchar(8000),
+	Lokale varchar(50),
+	Filter varchar(50),
+	Beschreibung varchar(500), 
+	WeitereBemerkungen varchar(500),
+	Treffer varchar(500)
+	)
+
+SELECT * FROM RESULTS
+
---
+++ LV_CATEGORISATION/Entities/LvCategorisationContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LV_CATEGORISATION.Entities;
+
+public partial class LvCategorisationContext : DbContext
+{
+    public LvCategorisationContext()
+    {
+    }
+
+    public LvCategorisationContext(DbContextOptions<LvCategorisationContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Result> Results { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS; Database=LV_CATEGORISATION; Integrated Security=True; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Result>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__RESULTS__3213E83F97979ADC");
+
+            entity.ToTable("RESULTS");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Beschreibung)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.Einheiten)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Filter)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Kurztext)
+                .HasMaxLength(1000)
+                .IsUnicode(false);
+            entity.Property(e => e.Langtext)
+                .HasMaxLength(8000)
+                .IsUnicode(false);
+            entity.Property(e => e.Lokale)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Menge).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Positionsnummer)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Treffer)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.WeitereBemerkungen)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
